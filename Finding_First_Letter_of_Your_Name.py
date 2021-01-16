d0e42816d70acf0dbb3ac5ba1e3be9580eec5044
Ｎａｒｕｔｏ Ｂｒａｔ <77386751+Naruto-Brat@users.noreply.github.com>
2021-01-16 15:36:45 +0545

    Add files via upload

---
+++ Finding_First_Letter_of_Your_Name.py
@@ -0,0 +1,4 @@
+# this code is just for fun i was learning coding so got nothing to upload it in github. Enjoy :)
+name = input("Enter Your Name :\n")
+print("The First Letter of your Name is ", name[0]) 
+# That's it now you can find first letter of your name :)
